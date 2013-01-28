0b2da3e63fbab811e8e8af875f9a1b9f5f4e1d09
Scott Talbot <s@chikachow.org>
2013-01-28 15:19:14 +1100

    Add tests skeleton

---
+++ STMobileProvisionTests/STMobileProvisionTests.h
@@ -0,0 +1,13 @@
+//
+//  STMobileProvisionTests.h
+//  STMobileProvision
+//
+//  Copyright (c) 2013 Scott Talbot. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface STMobileProvisionTests : SenTestCase
+
+@end
---
+++ STMobileProvisionTests/STMobileProvisionTests.m
@@ -0,0 +1,13 @@
+//
+//  STMobileProvisionTests.m
+//  STMobileProvision
+//
+//  Copyright (c) 2013 Scott Talbot. All rights reserved.
+//
+
+#import "STMobileProvisionTests.h"
+
+
+@implementation STMobileProvisionTests
+
+@end
